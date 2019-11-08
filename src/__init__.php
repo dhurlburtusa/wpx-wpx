13e5119ae93f9665ece414de40c3d59d5a7dedf1
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-11-08 12:01:51 -0700

    Update Version

--- src/__init__.php
+++ src/__init__.php
@@ -4,7 +4,7 @@ namespace Wpx;
 if ( ! defined( 'ABSPATH' ) ) { status_header(404); die(); }
 
 if ( ! defined( 'WPX__VERSION' ) ) {
-	define( 'WPX__VERSION', '0.0.0-alpha8' );
+	define( 'WPX__VERSION', '0.0.0-alpha10' );
 
 	function __404_and_die () {
 		status_header( 404 );
